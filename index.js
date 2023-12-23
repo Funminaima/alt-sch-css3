acd00e76b420735539569ca051408272b1ff356e
Naheemah Adeleke <66773854+Funminaima@users.noreply.github.com>
2023-12-23 09:03:20 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+let nav = `<nav id="mynav">
+
+        <a href="index.html" class="navlink ">About me</a>
+        <a href="form.html" class="navlink">Form</a>
+
+      </nav>`;
+const insertAfter = (el, htmlString) =>
+  el.insertAdjacentHTML("beforebegin", htmlString);
+
+insertAfter(document.getElementById("container"), nav);
+
+//add class active
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll("#mynav a");
+console.log(menuItem);
+
+for (i = 0; i < menuItem.length; i++) {
+  if (
+    currentLocation ===
+    "file:///Users/user/Documents/alt-sch-project/assignment3/form.html"
+  ) {
+    document.getElementById("mynav").style.width = "50%";
+  } else {
+    document.getElementById("mynav").style.width = "100%";
+  }
+  if (menuItem[i].href === currentLocation) {
+    menuItem[i].className = "active";
+    // console.log(menuItem[i].href, "menu item");
+  }
+}
