c1187fd3c50a90f7fe3f876b5baab42b19c382cf
Naheemah Adeleke <66773854+Funminaima@users.noreply.github.com>
2023-12-23 09:10:04 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let nav = `<nav id="mynav">
 
-        <a href="index.html" class="navlink ">About me</a>
+        <a href="index.html" class="navlink active ">About me</a>
         <a href="form.html" class="navlink">Form</a>
 
       </nav>`;
@@ -20,9 +20,7 @@ for (i = 0; i < menuItem.length; i++) {
     "file:///Users/user/Documents/alt-sch-project/assignment3/form.html"
   ) {
     document.getElementById("mynav").style.width = "50%";
-  } else {
-    document.getElementById("mynav").style.width = "100%";
-  }
+  } 
   if (menuItem[i].href === currentLocation) {
     menuItem[i].className = "active";
     // console.log(menuItem[i].href, "menu item");
